7f9efb471ad20872765aef85b0d52e344b936e1a
Otávio de Paula <otaviopaulaferreira@gmail.com>
2021-06-06 21:18:04 -0300

    Criação da tela Splash
    Não está pronta, precisa colocar o fundo gradiente. Fiz só pra ver como faz para trocar cor, adicionar imagem, ver como roda no celular e tals.

---
+++ App/app/src/main/java/app/otma/SplashActivity.java
@@ -0,0 +1,25 @@
+package app.otma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(getBaseContext(), MainActivity.class));
+            finish();
+        }, 5000);
+    }
+}
