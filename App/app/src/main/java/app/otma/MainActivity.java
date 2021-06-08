9d640066b4c5cd133fed5be7b95bf9523c5779eb
Otávio de Paula <otaviopaulaferreira@gmail.com>
2021-06-07 23:26:03 -0300

    O app não tá nem rodando

--- App/app/src/main/java/app/otma/MainActivity.java
+++ App/app/src/main/java/app/otma/MainActivity.java
@@ -1,14 +1,64 @@
 package app.otma;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navigationView=findViewById(R.id.NavigationView);
+        drawerLayout=findViewById(R.id.DrawerLayout);
+        toolbar=findViewById(R.id.app_Bar);
+
+        setSupportActionBar(toolbar);
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menuicon);
+
+        navigationView.setNavigationItemSelectedListener(menuItem -> {
+            switch (menuItem.getItemId())
+            {
+                case R.id.iteminicio:
+                    Toast.makeText(MainActivity.this, "Inicio", Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.itemcategorias:
+                    Toast.makeText(MainActivity.this, "Categorias", Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.itemcarriho:
+                    Toast.makeText(MainActivity.this, "Carrinho", Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.itemperfil:
+                    Toast.makeText(MainActivity.this, "Perfil", Toast.LENGTH_SHORT).show();
+                    return true;
+            }
+            return true;
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            drawerLayout.openDrawer(GravityCompat.START);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
