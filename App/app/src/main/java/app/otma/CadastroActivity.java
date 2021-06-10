4d64063dcfac935bec277ae21d8d32ad76174684
Maria Eduarda Gaspar <73003909+gasparmaria@users.noreply.github.com>
2021-06-10 14:14:57 -0300

    Update activity_cadastro.xml

--- App/app/src/main/java/app/otma/CadastroActivity.java
+++ App/app/src/main/java/app/otma/CadastroActivity.java
@@ -17,33 +17,9 @@ import app.otma.databinding.ActivityCadastroBinding;
 
 public class CadastroActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityCadastroBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        /*binding = ActivityCadastroBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        //setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_cadastro);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.view.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                        .setAction("Action", null).show();
-            }
-        });*/
+        setContentView(R.layout.activity_cadastro);
     }
-    /*
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_cadastro);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }*/
 }
--- App/app/src/main/java/app/otma/MainActivity.java
+++ App/app/src/main/java/app/otma/MainActivity.java
@@ -70,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void openNextActivity()
     {
-        Intent intent = new Intent(getApplicationContext(), IndexActivity.class);
+        Intent intent = new Intent(getApplicationContext(), CadastroActivity.class);
         startActivity(intent);
     }
 }
